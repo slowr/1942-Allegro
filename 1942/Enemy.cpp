c496bbbb324a7d3299fc8564e734ea46620c8e24
Dimitris Mavrommatis <blackeye_l2@yahoo.gr>
2016-02-02 16:23:46 +0200

    ?

--- 1942/Enemy.cpp
+++ 1942/Enemy.cpp
@@ -3,15 +3,16 @@
 #include <math.h>
 
 Enemy::Enemy(void) : Sprite(SCREEN_W / 2 - 32, SCREEN_H / 2, AnimationFilmHolder::Get().GetFilm("enemy.blue_plane"), spritetype_t::ENEMY){
-	//std::ostringstream oss;
+	std::ostringstream oss;
 	std::list<PathEntry *> p;
 	float px, py;
 	float oldDx = x;
 	float oldDy = y;
 	float radius = 200;
-	// oss << radius << std::endl;
+	oss << radius << std::endl;
 	float center_x = x - radius;
 	float center_y = y;
+	int frame = 0;
 	for (float degrees = 0; degrees <= 360; degrees++){
 		float radians = degrees*(M_PI / 180);
 		px = radius*cos(radians) + center_x;
@@ -22,25 +23,27 @@ Enemy::Enemy(void) : Sprite(SCREEN_W / 2 - 32, SCREEN_H / 2, AnimationFilmHolder
 		pE->dx = dx_move;
 		pE->dy = dy_move;
 
-		//oss << dx_move << ":" << dy_move << " ";
 		if (degrees >= 0 && degrees < 22.5)
-			pE->frame = 0;
+			frame = 0;
 		else if (degrees >= 22.5 && degrees < 67.5)
-			pE->frame = 7;
+			frame = 7;
 		else if (degrees >= 67.5 && degrees < 112.5)
-			pE->frame = 6;
+			frame = 6;
 		else if (degrees >= 112.5 && degrees < 157.5)
-			pE->frame = 5;
+			frame = 5;
 		else if (degrees >= 157.5 && degrees < 202.5)
-			pE->frame = 4;
+			frame = 4;
 		else if (degrees >= 202.5 && degrees < 247.5)
-			pE->frame = 3;
+			frame = 3;
 		else if (degrees >= 247.5 && degrees < 292.5)
-			pE->frame = 2;
+			frame = 2;
 		else if (degrees >= 292.5 && degrees < 337.5)
-			pE->frame = 1;
+			frame = 1;
 		else if (degrees >= 337.5 && degrees <= 360)
-			pE->frame = 0;
+			frame = 0;
+		
+		oss << dx_move << ":" << dy_move << ":" << frame << " " ;
+		pE->frame = frame;
 		pE->repetitions = 1;
 		pE->delay = 3000 / 360;
 		p.push_back(pE);
@@ -48,14 +51,14 @@ Enemy::Enemy(void) : Sprite(SCREEN_W / 2 - 32, SCREEN_H / 2, AnimationFilmHolder
 		oldDy = py;
 	}
 
-	//{
-	//	std::ofstream outFile;
-	//	std::string name = "resources/MovementStepsRadius.data";
-	//	oss << std::endl;
-	//	outFile.open(name.c_str(), std::ios_base::app);
-	//	outFile << oss.str() << std::endl;
-	//	outFile.close();
-	//}
+	{
+		std::ofstream outFile;
+		std::string name = "resources/MovementStepsRadius.data";
+		oss << std::endl;
+		outFile.open(name.c_str(), std::ios_base::app);
+		outFile << oss.str() << std::endl;
+		outFile.close();
+	}
 
 	animation = new MovingPathAnimation(p, 1);
 	animator = new MovingPathAnimator();
